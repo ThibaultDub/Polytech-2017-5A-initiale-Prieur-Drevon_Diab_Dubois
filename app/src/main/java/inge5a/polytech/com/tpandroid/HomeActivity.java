22c010089cd0ca2e0d82356851894b4578f043c3
Nicolas Diab <nicolas-diab@hotmail.fr>
2017-10-15 17:26:44 +0200

    Successfully corrected last merged. Everything is fine !

--- app/src/main/java/inge5a/polytech/com/tpandroid/HomeActivity.java
+++ app/src/main/java/inge5a/polytech/com/tpandroid/HomeActivity.java
@@ -1,13 +1,11 @@
 package inge5a.polytech.com.tpandroid;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.view.*;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AbstractActivity {
 
     private Button btn_1vs1;
     private Button btn_1vsAI;
@@ -17,13 +15,17 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
         btn_1vs1 = (Button) findViewById(R.id.btn_1Vs1);
         btn_1vsAI = (Button) findViewById(R.id.btn_1VsAi);
         btn_teamVsTeam = (Button) findViewById(R.id.btn_teamVsTeam);
         btn_1vs1Online = (Button) findViewById(R.id.btn_1Vs1Online);
     }
 
+    @Override
+    protected int getContent() {
+        return R.layout.activity_home;
+    }
 
     public void onClick(View v) {
         int id = v.getId();
