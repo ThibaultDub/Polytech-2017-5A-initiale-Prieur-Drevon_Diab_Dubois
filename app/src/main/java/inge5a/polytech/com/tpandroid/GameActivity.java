633616cb5ca7ffb083a914556c8a94c7845dfc94
Thibault Dubois <dudub74@gmail.com>
2017-10-18 11:22:23 +0200

    Working on IntentServiceQuestion, not working yet

--- app/src/main/java/inge5a/polytech/com/tpandroid/GameActivity.java
+++ app/src/main/java/inge5a/polytech/com/tpandroid/GameActivity.java
@@ -1,8 +1,54 @@
 package inge5a.polytech.com.tpandroid;
 
-public class GameActivity extends AbstractActivity{
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import inge5a.polytech.com.tpandroid.intent.IntentServiceQuestion;
+
+public class GameActivity extends AbstractActivity {
+    public class MyReceiver extends BroadcastReceiver {
+        public static final String ACTION_RESP = "inge5a.polytech.com.tpandroid.intent.action.TEXT_TO_DISPLAY";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra(IntentServiceQuestion.PARAM_OUT_MSG);
+            tv_question.setText(text);
+        }
+    }
+
+    private TextView tv_question;
+    private MyReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tv_question = (TextView) findViewById(R.id.tv_question);
+
+
+        IntentFilter filter = new IntentFilter(MyReceiver.ACTION_RESP);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
+
+
+        Intent msgIntent = new Intent(this, IntentServiceQuestion.class);
+        msgIntent.putExtra(IntentServiceQuestion.PARAM_IN_MSG, "new_question");
+        startService(msgIntent);
+
+
+
+    }
+
     @Override
     protected int getContent() {
         return R.layout.activity_game;
     }
+
+
 }
+
---
+++ app/src/main/java/inge5a/polytech/com/tpandroid/intent/IntentServiceQuestion.java
@@ -0,0 +1,41 @@
+package inge5a.polytech.com.tpandroid.intent;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import inge5a.polytech.com.tpandroid.GameActivity;
+
+/**
+ * Created by thibault on 10/18/17.
+ */
+
+public class IntentServiceQuestion extends IntentService {
+    public static final String PARAM_IN_MSG = "imsg";
+    public static final String PARAM_OUT_MSG = "omsg";
+
+
+    public IntentServiceQuestion(){
+        super("IntentServiceQuestion");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String current_request = intent.getStringExtra(PARAM_IN_MSG);
+
+        String resultTxt = "";
+        if (current_request == "new_question"){
+            resultTxt="Quelle est la couleur du cheval blanc d'henry IV ? //Blanc //Rouge //Multicolore//Parabolique";
+        }
+        else if (current_request == "check_answer"){
+            resultTxt="bien joué champion";
+        }
+
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(GameActivity.MyReceiver.ACTION_RESP);
+        broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        broadcastIntent.putExtra(PARAM_OUT_MSG, resultTxt);
+        sendBroadcast(broadcastIntent);
+
+    }
+}
